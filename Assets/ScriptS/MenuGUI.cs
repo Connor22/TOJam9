d8d34c86fc5bc39377d716ffbafa319a9bef796e
Connor22 <con_christopher@hotmail.com>
2014-04-26 16:26:26 -0400

    Title Screen
    Added Functional, Scaling Title Screen.

--- Assets/ScriptS/MenuGUI.cs
+++ Assets/ScriptS/MenuGUI.cs
@@ -1,38 +1,60 @@
-﻿using UnityEngine;
+using UnityEngine;
 using System.Collections;
 
 public class MenuGUI : MonoBehaviour {
+	//Top Banner
+	float topBannerH;
+	float topBannerW;
+	//Buttons
+	float buttonSizeH;
+	float buttonSizeW;
+	float buttonPos1;
+	float buttonPos2;
+	float buttonPos3;
+	float buttonPos4;
+	float buttonPos5;
+	//Bottom Banner
+	float bottomBannerH;
+	float bottomBannerW;
+	float bottomBannerPos;
+	string exampleVar1;
+	GUISkin customSkin1;
+	GUISkin customSkin2;
+	GUISkin customSkin3;
+	public Texture2D Play;
+	public Texture2D Instructions;
+	public Texture2D Credits;
+	public float Offset1;
+	public float Offset2;
 
-	int scrw = Screen.width;
-	int scrh = Screen.height;
-	GameObject titleText;
 
-	void Start () {
-		//titleText = new GameObject("Title Text");
-		titleText.AddComponent("GUIText");
-		titleText.guiText.text = "10,000 Sheep Under the Sea";
-		titleText.guiText.alignment = TextAlignment.Center;
-		titleText.guiText.pixelOffset = new Vector2 (scrw / 2 - scrw / 4, scrh / 2);
+
+
+	void Update () {	
+		topBannerH = Screen.height / 4;
+		topBannerW = Screen.width;
+		buttonSizeH = Screen.height/10;
+		buttonSizeW = Screen.width;
+		buttonPos1 = topBannerH;
+		buttonPos2 = topBannerH + buttonSizeH * Offset1;
+		buttonPos3 = topBannerH + buttonSizeH * Offset2;
+		bottomBannerH = Screen.height/4;
+		bottomBannerW = Screen.width;
+		bottomBannerPos = topBannerH + buttonSizeH*5;	
 	}
-
 	
-	void OnGUI () {
-		
-		
-		GUI.Box(new Rect(scrw / 2 - scrw / 12,scrh / 2 + scrh / 30,scrw / 6,scrh / 3 + scrh / 10), "Select Option");
-		
-		if(GUI.Button(new Rect(scrw / 2 - scrw / 16,scrh / 2 + scrh / 8,scrw / 8,scrw / 30), "Play")) {
+	void OnGUI () {		
+		if (GUI.Button(new Rect(0,buttonPos1,buttonSizeW,buttonSizeH), Play)){
 			Application.LoadLevel ("First Level");
 		}
-		
-		if(GUI.Button(new Rect(scrw / 2 - scrw / 16,scrh / 2 + 2 * scrh / 8,scrw / 8,scrw / 30), "Instruction")) {
+		//Button 2
+		if (GUI.Button(new Rect(0,buttonPos2,buttonSizeW,buttonSizeH), Instructions)){
 			print("Read it and weep");
 		}
-		
-		if(GUI.Button(new Rect(scrw / 2 - scrw / 16,scrh / 2 + 3 * scrh / 8,scrw / 8,scrw / 30), "Credits")) {
-			Application.LoadLevel ("Credits");
+		//Button 3
+		if (GUI.Button(new Rect(0,buttonPos3,buttonSizeW,buttonSizeH),Credits)){
+			Application.LoadLevel ("Credits");;
 		}
-
 	}
 	
 }
