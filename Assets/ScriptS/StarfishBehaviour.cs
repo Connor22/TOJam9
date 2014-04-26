6a6fc5fd247530723b56c28160a79a19c759198b
Sean-3ZX <triclopszx@gmail.com>
2014-04-26 14:06:14 -0400

    Torpedo fix
    it works now

---
+++ Assets/ScriptS/StarfishBehaviour.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StarfishBehaviour : MonoBehaviour {
+
+	public float speed;
+	public float force;
+	
+	void Start () {
+	
+	}
+
+	void Update () {
+	
+	}
+}
