927c5c5187f72dc853a0552cb57f55035e3dccf0
Sean-3ZX <triclopszx@gmail.com>
2014-04-25 20:52:31 -0400

    Credits and Renamed Menu
    Credits scene added and test renamed to Menu

---
+++ Assets/CreditsGUI.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CreditsGUI : MonoBehaviour {
+
+	void OnGUI () {
+		
+		if(GUI.Button(new Rect(Screen.width / 2 - 40,320,80,20), "Back")) {
+			Application.LoadLevel ("Menu");
+		}
+		
+	}
+}
--- Assets/MenuGUI.cs
+++ Assets/MenuGUI.cs
@@ -10,7 +10,7 @@ public class GuiTest : MonoBehaviour {
 		GUI.Box(new Rect(Screen.width / 2 - 50,210,100,140), "Select Option");
 
 		if(GUI.Button(new Rect(Screen.width / 2 - 40,240,80,20), "Play")) {
-			print("Aw hell naw");
+			Application.LoadLevel ("First Level");
 		}
 
 		if(GUI.Button(new Rect(Screen.width / 2 - 40,280,80,20), "Instruction")) {
@@ -18,7 +18,7 @@ public class GuiTest : MonoBehaviour {
 		}
 
 		if(GUI.Button(new Rect(Screen.width / 2 - 40,320,80,20), "Credits")) {
-			print("It's all me");
+			Application.LoadLevel ("Credits");
 		}
 
 	}
