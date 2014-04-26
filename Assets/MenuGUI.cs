a6d81bd4c76965ed714b88effc11c6277bb98fb6
Sean-3ZX <triclopszx@gmail.com>
2014-04-25 21:22:49 -0400

    Menu stuff
    yeah

--- Assets/MenuGUI.cs
+++ Assets/MenuGUI.cs
@@ -1,26 +1,29 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class GuiTest : MonoBehaviour {
-
-
+public class MenuGUI : MonoBehaviour {
+	
+	
 	void OnGUI () {
 
-
-		GUI.Box(new Rect(Screen.width / 2 - 50,210,100,140), "Select Option");
-
-		if(GUI.Button(new Rect(Screen.width / 2 - 40,240,80,20), "Play")) {
+		int scrw = Screen.width;
+		int scrh = Screen.height;
+		
+		
+		GUI.Box(new Rect(scrw / 2 - scrw / 12,scrh / 2 + scrh / 50,scrw / 6,scrh / 3), "Select Option");
+		
+		if(GUI.Button(new Rect(scrw / 2 - 40,scrh / 2 + scrh / 10,80,20), "Play")) {
 			Application.LoadLevel ("First Level");
 		}
-
-		if(GUI.Button(new Rect(Screen.width / 2 - 40,280,80,20), "Instruction")) {
+		
+		if(GUI.Button(new Rect(scrw / 2 - 40,scrh / 2 + 2 * scrh / 10,80,20), "Instruction")) {
 			print("Read it and weep");
 		}
-
-		if(GUI.Button(new Rect(Screen.width / 2 - 40,320,80,20), "Credits")) {
+		
+		if(GUI.Button(new Rect(scrw / 2 - 40,scrh / 2 + 3 * scrh / 10,80,20), "Credits")) {
 			Application.LoadLevel ("Credits");
 		}
-
+		
 	}
-
+	
 }
