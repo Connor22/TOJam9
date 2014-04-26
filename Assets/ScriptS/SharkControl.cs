c2e512759bfcc8b09ab19b4dd35a75c77149c199
bsen <b.sen@mail.utoronto.ca>
2014-04-26 18:59:24 -0400

    Shark Energy Limits
    Gives the shark regenerating energy that is used to fire projectiles.

--- Assets/ScriptS/SharkControl.cs
+++ Assets/ScriptS/SharkControl.cs
@@ -6,11 +6,18 @@ public class SharkControl : MonoBehaviour {
 	public float force = 100.0F;
 	public float bombForce = 50.0f;
 	public GameObject Torpedo;
+	public float torpedoCost = 5.0f;
 	public GameObject Bomb;
+	public float bombCost = 10.0f;
 	public GameObject Follow;
+	public float followCost = 30.0f;
 	public GameObject Sin;
+	public float sinCost = 15.0f;
 	public int SetBulletDelay;
-
+	public float energy = 100f;
+	public float maximumEnergy = 200f;
+	public float energyPerSecond = 5f;
+	
 	private Vector3 BulletPos;
 	private int BulletTime = 0;
 	// Use this for initialization
@@ -21,6 +28,12 @@ public class SharkControl : MonoBehaviour {
 	void Update () {
 		// movement
 		rigidbody2D.AddForce (new Vector2 (0f, force * Input.GetAxis ("P3Vertical")));
+		
+		// energy tracking
+		energy += Time.deltaTime * energyPerSecond;
+		if (energy > maximumEnergy) {
+			energy = maximumEnergy;
+		}
 
 		// firing
 		BulletPos = new Vector3(transform.position.x + 7f, transform.position.y);
@@ -28,26 +41,32 @@ public class SharkControl : MonoBehaviour {
 			BulletTime -= 1;
 		}
 
-		if ((BulletTime == 0) && (Input.GetAxis("SharkFireTorpedo") > 0)) {
+		if ((BulletTime == 0) && (energy >= torpedoCost) && (Input.GetAxis("SharkFireTorpedo") > 0)) {
 			Instantiate(Torpedo, BulletPos, transform.rotation);
 
+			energy -= torpedoCost;
 			BulletTime = SetBulletDelay;
 		}
-		if ((BulletTime == 0) && (Input.GetAxis("SharkFireBomb") > 0)) {
+		if ((BulletTime == 0) && (energy >= bombCost) && (Input.GetAxis("SharkFireBomb") > 0)) {
 			GameObject bomb = Instantiate(Bomb, BulletPos, transform.rotation) as GameObject;
 			bomb.rigidbody2D.AddForce(new Vector2(bombForce + Random.Range(-bombForce * 0.2f, bombForce * 0.2f), 0f)); // throw bomb with some force variance
 			bomb.GetComponent<BombBehaviour>().canExplode = true;
 
+			energy -= bombCost;
 			BulletTime = SetBulletDelay;
 		}
 
-		if ((BulletTime == 0) && (Input.GetAxis("SharkFireSin") > 0)) {
+		if ((BulletTime == 0) && (energy >= sinCost) && (Input.GetAxis("SharkFireSin") > 0)) {
 			Instantiate(Sin, BulletPos, transform.rotation);
+
+			energy -= sinCost;
 			BulletTime = SetBulletDelay;
 		}
 
-		if ((BulletTime == 0) && (Input.GetAxis("SharkFireFollow") > 0)) {
+		if ((BulletTime == 0) && (energy >= followCost) && (Input.GetAxis("SharkFireFollow") > 0)) {
 			Instantiate(Follow, BulletPos, transform.rotation);
+
+			energy -= followCost;
 			BulletTime = SetBulletDelay;
 		}
 		// end firing
