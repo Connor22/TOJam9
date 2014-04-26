5bc5345e494503c636770b3aaf68855e8da37304
Sean-3ZX <triclopszx@gmail.com>
2014-04-26 17:33:46 -0400

    Sine Enemy
    yeah

---
+++ Assets/ScriptS/SineBehaviour.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SineBehaviour : MonoBehaviour {
+
+	public float speed; // Set to 0.1
+	public float amplitude; // Set to 2
+	public float period; // Set to 3
+
+	float originalY;
+	Vector3 newPosition;
+
+	void Start () {
+		originalY = transform.position.y;
+
+	}
+
+	void Update () {
+
+		newPosition = new Vector3(transform.position.x + speed, 
+		                          amplitude * (originalY + Mathf.Sin (Time.time * period)), 0f);
+		transform.position = newPosition;
+
+	}
+
+	void OnCollisionEnter2D (Collision2D collision) {
+		Destroy (gameObject);
+	}
+}
