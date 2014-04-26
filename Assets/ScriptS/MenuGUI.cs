1ba2b206e8bc257b8287d84ad9297648b65a2714
Sean-3ZX <triclopszx@gmail.com>
2014-04-25 21:45:43 -0400

    Menu complete?
    added title and fixed buttons

--- Assets/ScriptS/MenuGUI.cs
+++ Assets/ScriptS/MenuGUI.cs
@@ -5,25 +5,25 @@ public class MenuGUI : MonoBehaviour {
 	
 	
 	void OnGUI () {
-
+		
 		int scrw = Screen.width;
 		int scrh = Screen.height;
 		
 		
-		GUI.Box(new Rect(scrw / 2 - scrw / 12,scrh / 2 + scrh / 50,scrw / 6,scrh / 3), "Select Option");
+		GUI.Box(new Rect(scrw / 2 - scrw / 12,scrh / 2 + scrh / 30,scrw / 6,scrh / 3 + scrh / 10), "Select Option");
 		
-		if(GUI.Button(new Rect(scrw / 2 - 40,scrh / 2 + scrh / 10,80,20), "Play")) {
+		if(GUI.Button(new Rect(scrw / 2 - scrw / 16,scrh / 2 + scrh / 8,scrw / 8,scrw / 30), "Play")) {
 			Application.LoadLevel ("First Level");
 		}
 		
-		if(GUI.Button(new Rect(scrw / 2 - 40,scrh / 2 + 2 * scrh / 10,80,20), "Instruction")) {
+		if(GUI.Button(new Rect(scrw / 2 - scrw / 16,scrh / 2 + 2 * scrh / 8,scrw / 8,scrw / 30), "Instruction")) {
 			print("Read it and weep");
 		}
 		
-		if(GUI.Button(new Rect(scrw / 2 - 40,scrh / 2 + 3 * scrh / 10,80,20), "Credits")) {
+		if(GUI.Button(new Rect(scrw / 2 - scrw / 16,scrh / 2 + 3 * scrh / 8,scrw / 8,scrw / 30), "Credits")) {
 			Application.LoadLevel ("Credits");
 		}
-		
+
 	}
 	
 }
