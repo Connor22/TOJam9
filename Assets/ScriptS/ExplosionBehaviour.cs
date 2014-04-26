3699bd7f2e2b3e8f42483d630cfeaaa5c07a307b
bsen <b.sen@mail.utoronto.ca>
2014-04-26 17:44:10 -0400

    Explosions Improved
    Tweaked explosion behaviour to prevent the invisible blast from sticking
    around for too long.

--- Assets/ScriptS/ExplosionBehaviour.cs
+++ Assets/ScriptS/ExplosionBehaviour.cs
@@ -4,8 +4,10 @@ using System.Collections;
 public class ExplosionBehaviour : MonoBehaviour {
 
 	public float lifetime = 3f;
+	public float blastLifetime = 0.5f;
 
 	private float remainingLifetime;
+	private bool blastPresent = true;
 
 	// Use this for initialization
 	void Start () {
@@ -18,6 +20,11 @@ public class ExplosionBehaviour : MonoBehaviour {
 
 		if (remainingLifetime < 0) {
 			Destroy(gameObject);
+		}
+
+		if (blastPresent && ((remainingLifetime + blastLifetime) < lifetime)) {
+			// blast lifetime has expired
+			Destroy(rigidbody2D);
 		}
 	}
 }
