303c2254f10b1b953c3ffd728e2d636977edfa05
Sean-3ZX <triclopszx@gmail.com>
2014-04-26 18:25:36 -0400

    Fixed Sine
    should work now

--- Assets/ScriptS/SineBehaviour.cs
+++ Assets/ScriptS/SineBehaviour.cs
@@ -5,12 +5,13 @@ public class SineBehaviour : MonoBehaviour {
 
 	public float speed; // Set to 0.1
 	public float amplitude; // Set to 2
-	public float period; // Set to 3
 
 	float originalY;
+	float originalX;
 	Vector3 newPosition;
 
 	void Start () {
+		originalX = transform.position.x;
 		originalY = transform.position.y;
 
 	}
@@ -18,7 +19,8 @@ public class SineBehaviour : MonoBehaviour {
 	void Update () {
 
 		newPosition = new Vector3(transform.position.x + speed, 
-		                          amplitude * (originalY + Mathf.Sin (transform.position.x * period)), 0f);
+		                          amplitude * (originalY + 
+		                          Mathf.Sin (transform.position.x - originalX)), 0f);
 		transform.position = newPosition;
 
 	}
