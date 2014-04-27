c28a324dcac32cd89ab1b7bcc16c3bb089406db0
Connor22 <con_christopher@hotmail.com>
2014-04-27 15:03:05 -0400

    Credits Page!

---
+++ Assets/CreditsScroll.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CreditsScroll : MonoBehaviour {
+	public float timeElapsed;
+	public float waitTime;
+	public float stopTime;
+	public float speed;
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		timeElapsed += Time.deltaTime;
+		if (timeElapsed > waitTime){
+			rigidbody2D.velocity = new Vector2 (0f, speed);
+		}
+		if (timeElapsed > stopTime){
+			rigidbody2D.velocity = new Vector2 (0f, 0f);
+		}
+	}
+}
