c2b8f029e4d2599847c5e62e908a6b19442e8705
Sean-3ZX <triclopszx@gmail.com>
2014-04-27 13:16:19 -0400

    Music and possible invincibility frame
    Need to test invincibility though

--- Assets/ScriptS/SharkHealth.cs
+++ Assets/ScriptS/SharkHealth.cs
@@ -2,33 +2,51 @@
 using System.Collections;
 
 public class SharkHealth : MonoBehaviour {
+
 	public float SharkHPMax = 100;
 	public Texture2D LifeBarFull;
 	public Texture2D LifeBarHurt;
 	public Texture2D Shark1;
 	public float SharkHPCur;
+	public int invincible;
 
 
 	private PlayerHealth LifeRef;
 	private float LifeWidth;
+	private float invCounter;
 
 	// Use this for initialization
 	void Start () {
 		SharkHPCur = SharkHPMax;
+		invCounter = -1f;
 	}
 	
 	// Update is called once per frame
 	void Update () {
+		if (invCounter > -1){
+			if (invCounter > invincible){
+				renderer.enabled = true;
+				invCounter = -1f;
+			} else if (renderer.enabled == true){
+				renderer.enabled = false;
+				invCounter += Time.deltaTime;
+			} else {
+				renderer.enabled = false;
+				invCounter += Time.deltaTime;
+			}
 	}
 
 	void OnCollisionEnter2D(Collision2D collision){
-		SharkHPCur -= 10;
-		LifeRef = GameObject.Find("Players").GetComponent<PlayerHealth>();
-		if (SharkHPCur == 0.0){
-			if(LifeRef.PlayerCount == 0){
-				Application.LoadLevel ("Draw Screen");
-			} else {
-				Application.LoadLevel ("Sheep Win");
+		if (collision.gameObject.tag != "Bounds" && invCounter > -1) {
+			SharkHPCur -= 10;
+			invCounter = 0f;
+			LifeRef = GameObject.Find ("Players").GetComponent<PlayerHealth> ();
+			if (SharkHPCur == 0.0) {
+					if (LifeRef.PlayerCount == 0) {
+							Application.LoadLevel ("Draw Screen");
+					} else {
+							Application.LoadLevel ("Sheep Win");
+					}
 			}
 		}
 	}
