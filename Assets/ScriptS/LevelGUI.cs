933041a4e45dc9b87daae3d717fe3105178736a5
Connor22 <con_christopher@hotmail.com>
2014-04-27 06:50:58 -0400

    SECOND TRY
    -Background is here!
    
    -Shark fires from his mouth!
    
    -Improved Health and Energy Bars!
    
    -All-New Ending and Title Screens!
    
    -Animations!
    
    -Tweaked Sine Enemy movement!
    
    -Bomb Explosion radius tweaked.

---
+++ Assets/ScriptS/LevelGUI.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelGUI : MonoBehaviour {
+
+	public int healthBarGUIHeight = 32;
+	public int healthBarGUIWidth = 128;
+
+	private SharkHealth sharkHealthRef;
+	private float fullHP;
+	private GameObject healthBarRef;
+	private Vector3 healthBarFullSize;
+
+	public int energyBarGUIHeight = 32;
+	public int energyBarGUIWidth = 128;
+
+	private SharkControl sharkControlRef;
+	private float fullEnergy;
+	private GameObject energyBarRef;
+	private Vector3 energyBarFullSize;
+
+	private PlayerHealth playerHealthRef;
+	private int fullLives;
+
+	// Use this for initialization
+	void Start () {
+		sharkHealthRef = GameObject.Find("Shark").GetComponent<SharkHealth>();
+		fullHP = sharkHealthRef.SharkHPMax;
+		healthBarRef = GameObject.Find("Health Bar Foreground");
+		healthBarFullSize = healthBarRef.transform.localScale;
+
+		sharkControlRef = sharkHealthRef.GetComponent<SharkControl>();
+		fullEnergy = sharkControlRef.maximumEnergy;
+		energyBarRef = GameObject.Find("Energy Bar Foreground");
+		energyBarFullSize = energyBarRef.transform.localScale;
+
+		playerHealthRef = GameObject.Find("Players").GetComponent<PlayerHealth>();
+		fullLives = playerHealthRef.LifePool;
+	}
+	
+	void OnGUI () {
+		float currentHP = sharkHealthRef.SharkHPCur;
+		float currentEnergy = sharkControlRef.energy;
+		int currentLives = playerHealthRef.LifePool;
+
+		// size the foregrounds appropriately
+		healthBarRef.transform.localScale = new Vector3(healthBarFullSize.x * ((float)currentHP / (float)fullHP), healthBarFullSize.y);
+		energyBarRef.transform.localScale = new Vector3(energyBarFullSize.x * (currentEnergy / fullEnergy), energyBarFullSize.y);
+
+		// show numbers
+		GUI.Box(new Rect(0, 0, (float)healthBarGUIWidth, (float)healthBarGUIHeight), 
+		        currentHP.ToString() + " / " + fullHP.ToString() + " Health"); // health at the top left
+		GUI.Box(new Rect(0, Screen.height - energyBarGUIHeight, (float)energyBarGUIWidth, (float)energyBarGUIHeight), 
+		        currentEnergy.ToString("####") + " / " + fullEnergy.ToString("####") + " Energy"); // energy at the bottom left
+		GUI.Box(new Rect(Screen.width - healthBarGUIWidth, 0, (float)healthBarGUIWidth, (float)healthBarGUIHeight), 
+		        currentLives.ToString() + " / " + fullLives.ToString() + " Lives"); // lives at the top right
+	}
+}
---
+++ Assets/ScriptS/SineBehaviour2.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SineBehaviour2 : MonoBehaviour {
+
+	private float originalY;
+	private float currentRange;
+	public float amplitude;
+	public float speed;
+	public float appliedForce;
+
+	void Start(){
+		originalY = transform.position.y;		
+		}
+
+	void Update() {
+		currentRange = Mathf.Sin (originalY + amplitude * Time.time);
+		if (currentRange > 0) {
+			rigidbody2D.AddForce(new Vector2(speed, appliedForce));		
+		}
+		if (currentRange < 0) {
+			rigidbody2D.AddForce (new Vector2 (speed, -appliedForce));	
+		} 
+		else {
+			rigidbody2D.velocity = new Vector2 (speed, 0f);	
+		}
+	}
+
+	void OnCollisionEnter2D (Collision2D collision) {
+		Destroy (gameObject);
+	}
+}
